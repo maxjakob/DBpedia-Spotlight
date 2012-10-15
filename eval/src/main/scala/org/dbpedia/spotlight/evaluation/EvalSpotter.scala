5bd4aa7ecfde3f5a69a4f003887b4acd3c2bbdba
Max Jakob <max.jakob@gmail.com>
2012-10-15 16:59:39 +0200

    re-factored EvalSpotter to include more spotters
    - deleted EvalSpotterAhoCorasick, now obsolete

--- eval/src/main/scala/org/dbpedia/spotlight/evaluation/EvalSpotter.scala
+++ eval/src/main/scala/org/dbpedia/spotlight/evaluation/EvalSpotter.scala
@@ -8,26 +8,51 @@ import org.dbpedia.spotlight.spot.lingpipe.LingPipeSpotter
 import com.aliasi.dict.{DictionaryEntry, MapDictionary}
 import org.apache.lucene.analysis.en.EnglishAnalyzer
 import org.apache.lucene.util.Version
-import org.dbpedia.spotlight.model.{Factory, SurfaceFormOccurrence}
+import org.dbpedia.spotlight.model.{SurfaceForm, Factory, SurfaceFormOccurrence}
 import collection.JavaConversions
 import org.apache.lucene.analysis._
 import org.apache.commons.logging.LogFactory
 import org.apache.lucene.analysis.standard.{StandardAnalyzer, ClassicAnalyzer}
+import org.dbpedia.spotlight.spot.ahocorasick.AhoCorasickSpotter
 
 /**
+ * This class evaluates spotters by taking an annotated corpus, indexing its surface forms,
+ * and checking how many surface forms are found by a number spotters.
  *
- *
+ * Set the corpus in evalCorpus.
+ * Set the spotters in spotterMethods.
  */
 object EvalSpotter {
 
   private val LOG = LogFactory.getLog(this.getClass)
 
-  def main(args: Array[String]) {
-    evalSpotting(MilneWittenCorpus.fromDirectory(new File("/home/max/spotlight-data/milne-witten")))
-    //evalSpotting(AnnotatedTextSource.fromOccurrencesFile(new File("/home/max/spotlight-data/CSAWoccs.red-dis-3.7-sorted.tsv")))
+  def evalCorpus = {
+    MilneWittenCorpus.fromDirectory(new File("/home/max/spotlight-data/milne-witten"))
+    //AnnotatedTextSource.fromOccurrencesFile(new File("/home/max/spotlight-data/CSAWoccs.red-dis-3.7-sorted.tsv")))
   }
 
-  def evalSpotting(annotatedTextSource: AnnotatedTextSource) {
+  def spotterMethods: List[Traversable[SurfaceForm] => Spotter] = {
+    getLingPipeSpotters :::
+        getAhoCorasickSpotter
+  }
+
+  def main(args: Array[String]) {
+    val expected = getExpectedResult(evalCorpus)
+    for (spotter <- spotterMethods) {
+      evalSpotting(evalCorpus, spotter, expected)
+    }
+  }
+
+
+  private def getAhoCorasickSpotter: List[Traversable[SurfaceForm] => Spotter] = {
+    List({
+      sfs: Traversable[SurfaceForm] =>
+        AhoCorasickSpotter.fromSurfaceForms(sfs.map(_.name), caseSensitive = false, overlap = false)
+    })
+  }
+
+  private def getLingPipeSpotters: List[Traversable[SurfaceForm] => Spotter] = {
+    // LingPipe with different analyzers
     val analyzers = List(
       new SimpleAnalyzer(Version.LUCENE_36),
       new StopAnalyzer(Version.LUCENE_36),
@@ -36,33 +61,43 @@ object EvalSpotter {
       new EnglishAnalyzer(Version.LUCENE_36),
       new WhitespaceAnalyzer(Version.LUCENE_36)
     )
-    for (analyzer <- analyzers) {
-      evalSpotting(annotatedTextSource, analyzer)
+    analyzers.map(analyzer => {
+      val analyzerName = analyzer.getClass.toString.replaceFirst("^.*\\.", "")
+      surfaceForms: Traversable[SurfaceForm] =>  {
+        val dictionary = new MapDictionary[String]()
+        for (surfaceForm <- surfaceForms) {
+          dictionary.addEntry(new DictionaryEntry[String](surfaceForm.name, ""))
+        }
+        val lps = new LingPipeSpotter(dictionary, analyzer, false, false)
+        lps.setName("LingPipeSpotter[analyzer=%s]".format(analyzerName))
+        lps
+      }
+    })
+  }
+
+  private def getExpectedResult(annotatedTextSource: AnnotatedTextSource) = {
+    annotatedTextSource.foldLeft(Set[SurfaceFormOccurrence]()){ (set, par) =>
+      set ++ par.occurrences.map(Factory.SurfaceFormOccurrence.from(_))
     }
   }
 
-  def evalSpotting(annotatedTextSource: AnnotatedTextSource, analyzer: Analyzer) {
-    // create gold standard and index
-    var expected = Set[SurfaceFormOccurrence]()
-    val dictionary = new MapDictionary[String]()
-    for (paragraph <- annotatedTextSource;
-         occ <- paragraph.occurrences) {
-      expected += Factory.SurfaceFormOccurrence.from(occ)
-      dictionary.addEntry(new DictionaryEntry[String](occ.surfaceForm.name, ""))
-    }
-    val lingPipeSpotter: Spotter = new LingPipeSpotter(dictionary, analyzer)
+  private def evalSpotting(annotatedTextSource: AnnotatedTextSource,
+                   indexSpotter: Traversable[SurfaceForm] => Spotter,
+                   expected: Traversable[SurfaceFormOccurrence]) {
+    // index spotter
+    val spotter = indexSpotter(expected.map(_.surfaceForm))
 
-    // eval
+    // run spotting
     var actual = Set[SurfaceFormOccurrence]()
     for (paragraph <- annotatedTextSource) {
-      actual = JavaConversions.asScalaBuffer(lingPipeSpotter.extract(paragraph.text)).toSet union actual
+      actual = JavaConversions.asScalaBuffer(spotter.extract(paragraph.text)).toSet union actual
     }
 
-    printResults("LingPipeSpotter with %s and corpus %s".format(analyzer.getClass, annotatedTextSource.name),
-      expected, actual)
+    // compare
+    printResults("%s and corpus %s".format(spotter.getName, annotatedTextSource.name), expected, actual)
   }
 
-  def printResults(description: String, expected: Set[SurfaceFormOccurrence], actual: Set[SurfaceFormOccurrence]) {
+  private def printResults(description: String, expected: Traversable[SurfaceFormOccurrence], actual: Set[SurfaceFormOccurrence]) {
     var truePositive = 0
     var falseNegative = 0
     for (e <- expected) {
@@ -79,9 +114,9 @@ object EvalSpotter {
     val recall = truePositive.toDouble / (truePositive + falsePositive)
 
     LOG.info(description)
-    LOG.info(" | actual Y | actual N")
-    LOG.info("expected Y | %3d | %3d".format(truePositive, falseNegative))
-    LOG.info("expected N | %3d | N/A".format(falsePositive))
+    LOG.info("           | actual Y | actual N")
+    LOG.info("expected Y |   %3d    | %3d".format(truePositive, falseNegative))
+    LOG.info("expected N |   %3d    | N/A".format(falsePositive))
     LOG.info("precision: %f recall: %f".format(precision, recall))
     LOG.info("--------------------------------")
   }
